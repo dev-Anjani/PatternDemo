aef7235e1ffe830a1114ba104b3fb1c43490f578
Anjani <anjaniakb@gmail.com>
2020-07-02 00:08:58 +0530

    Add project files.

---
+++ SingletonPattern/Controllers/LoggerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingletonPattern.Controllers
+{
+    public class LoggerController : Controller
+    {
+        // GET: Logger
+        Singleton obj1 = Singleton.GetInstance;
+        
+        public ActionResult Index()
+        {
+            obj1.Logger("");
+            return View();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private Singleton() { counter++; }
+
+        ////Lazy Loading ... but problem with multiple thread
+        //private Singleton instance = null;
+
+        //public Singleton GetInstance
+        //{
+        //    get
+        //    {
+        //        if (instance == null) instance = new Singleton(); 
+        //        return instance;
+        //    }
+        //}
+
+
+        ////Eager Loading ... but problem with multiple thread
+        //private Singleton instance = new Singleton();
+
+        //public Singleton GetInstance
+        //{
+        //    get
+        //    {
+        //        return instance;
+        //    }
+        //}
+
+        //Eager Loading 
+        private static Lazy<Singleton> instance = new Lazy<Singleton>();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void Logger(string msg)
+        {
+            string fileName = string.Format($"{"Exception"}_{DateTime.Now}.log");
+            string logFilePath = string.Format($@"{AppDomain.CurrentDomain.BaseDirectory}\{fileName}");
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("---------------------------------------------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(msg);
+
+            using (StreamWriter writer = new StreamWriter(logFilePath,true))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
+        }
+    }
+}
